9d386855c1ef934a90bef2ccde5bc164991df580
zhaohejing2017 <zhaohejing@zhj125.cn>
2018-03-16 14:19:54 +0800

    ww

--- src/ItMonkey.Application/Shufflings/Dtos/ShufflingEditDto.cs
+++ src/ItMonkey.Application/Shufflings/Dtos/ShufflingEditDto.cs
@@ -6,11 +6,10 @@ namespace ItMonkey.Shufflings.Dtos
 {
     public class ShufflingEditDto
     {
-        ////BCC/ BEGIN CUSTOM CODE SECTION
-        ////ECC/ END CUSTOM CODE SECTION
         public int? Id { get; set; }
         public string Title { get; set; }
         public string ImageUrl { get; set; }
+        public int Sort { get; set; }
         public string Path { get; set; }
         public bool IsActive { get; set; }
     }
